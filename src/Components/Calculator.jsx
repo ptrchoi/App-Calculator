1f50601e5c1b975f941efd15d06ae511b70abe68
Peter Choi <ptrchoi@gmail.com>
2020-10-14 14:46:59 -0500

    removed console.log for calc

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -6,223 +6,222 @@ import Inputs from './Inputs';
 
 // LOCAL FUNCTIONS
 function convertArrToString(arr) {
-	let str = arr.toString();
-	str = str.replace(/,/g, ' ');
+  let str = arr.toString();
+  str = str.replace(/,/g, ' ');
 
-	return str;
+  return str;
 }
 function calculate(x, op, y, ignoreOp) {
-	let formulaStr = '';
-	if (ignoreOp) formulaStr = convertArrToString([ x, y ]);
-	else formulaStr = convertArrToString([ x, op, y ]);
+  let formulaStr = '';
+  if (ignoreOp) formulaStr = convertArrToString([x, y]);
+  else formulaStr = convertArrToString([x, op, y]);
 
-	let answer = eval(formulaStr);
-	return {
-		formula: formulaStr + ' = ' + answer,
-		answer: answer
-	};
+  let answer = eval(formulaStr);
+  return {
+    formula: formulaStr + ' = ' + answer,
+    answer: answer,
+  };
 }
 
 // CALCULATOR COMPONENT CLASS
 class Calculator extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			operationQueue: [],
-			curOperation: {
-				operand1: 0,
-				operator: 'none',
-				operand2: 0
-			},
-			calcMem: {
-				result: null,
-				prevInput: null,
-				prevOperator: 'none',
-				prevOperand: null,
-				calcTape: []
-			}
-		};
+    this.state = {
+      operationQueue: [],
+      curOperation: {
+        operand1: 0,
+        operator: 'none',
+        operand2: 0,
+      },
+      calcMem: {
+        result: null,
+        prevInput: null,
+        prevOperator: 'none',
+        prevOperand: null,
+        calcTape: [],
+      },
+    };
 
-		this.handleDigitInput = this.handleDigitInput.bind(this);
-		this.handleOperation = this.handleOperation.bind(this);
-		this.handleEquals = this.handleEquals.bind(this);
-		this.handleModifiedResult = this.handleModifiedResult.bind(this);
-		this.handleClear = this.handleClear.bind(this);
-		this.handleClearTape = this.handleClearTape.bind(this);
-	}
-	// For input sequence tracking
-	handleDigitInput() {
-		let { calcTape } = this.state.calcMem;
-		this.setState({
-			calcMem: {
-				prevInput: 'number',
-				calcTape: calcTape
-			}
-		});
-	}
-	handleOperation(op, operand) {
-		let { operationQueue, curOperation, calcMem } = this.state;
-		let { operand1, operator } = curOperation;
-		let { result, prevInput, calcTape } = calcMem;
+    this.handleDigitInput = this.handleDigitInput.bind(this);
+    this.handleOperation = this.handleOperation.bind(this);
+    this.handleEquals = this.handleEquals.bind(this);
+    this.handleModifiedResult = this.handleModifiedResult.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+    this.handleClearTape = this.handleClearTape.bind(this);
+  }
+  // For input sequence tracking
+  handleDigitInput() {
+    let { calcTape } = this.state.calcMem;
+    this.setState({
+      calcMem: {
+        prevInput: 'number',
+        calcTape: calcTape,
+      },
+    });
+  }
+  handleOperation(op, operand) {
+    let { operationQueue, curOperation, calcMem } = this.state;
+    let { operand1, operator } = curOperation;
+    let { result, prevInput, calcTape } = calcMem;
 
-		// Edge case1: [op] directly after [=]
-		if (prevInput === 'equals') {
-			operand1 = result;
-			operationQueue.push(operand1, op);
-		} else {
-			if (prevInput === 'operator') {
-				// Edge case2: [op] directly after [op]
-				// replace end of queue (previous op)
-				operationQueue.splice(operationQueue.length - 1, 1, op);
-			} else {
-				// Else just add new operand and operation as normal
-				operationQueue.push(operand, op);
+    // Edge case1: [op] directly after [=]
+    if (prevInput === 'equals') {
+      operand1 = result;
+      operationQueue.push(operand1, op);
+    } else {
+      if (prevInput === 'operator') {
+        // Edge case2: [op] directly after [op]
+        // replace end of queue (previous op)
+        operationQueue.splice(operationQueue.length - 1, 1, op);
+      } else {
+        // Else just add new operand and operation as normal
+        operationQueue.push(operand, op);
 
-				// Start new calculation
-				if (operator === 'none') operand1 = operand;
-				else
-					// Else Replace operand1 w/ operationQueue
-					operand1 = operationQueue;
-			}
-		}
-		this.setState({
-			operationQueue: operationQueue,
-			curOperation: {
-				operand1: operand1,
-				operator: op
-			},
-			calcMem: {
-				result: result,
-				prevInput: 'operator',
-				calcTape: calcTape
-			}
-		});
-	}
-	handleEquals(operand) {
-		let { curOperation, calcMem } = this.state;
-		let { operand1, operator, operand2 } = curOperation;
-		let { result, prevInput, prevOperator, prevOperand, calcTape } = calcMem;
-		let performCalculation = true;
+        // Start new calculation
+        if (operator === 'none') operand1 = operand;
+        // Else Replace operand1 w/ operationQueue
+        else operand1 = operationQueue;
+      }
+    }
+    this.setState({
+      operationQueue: operationQueue,
+      curOperation: {
+        operand1: operand1,
+        operator: op,
+      },
+      calcMem: {
+        result: result,
+        prevInput: 'operator',
+        calcTape: calcTape,
+      },
+    });
+  }
+  handleEquals(operand) {
+    let { curOperation, calcMem } = this.state;
+    let { operand1, operator, operand2 } = curOperation;
+    let { result, prevInput, prevOperator, prevOperand, calcTape } = calcMem;
+    let performCalculation = true;
 
-		// Edge case1: New calculation of [=] directly after [num] with no operator
-		if (operator === 'none' && prevOperator === undefined) {
-			operator = prevOperator;
-			operand2 = prevOperand;
-			result = operand;
-			performCalculation = false;
-		} else if (prevInput === 'operator') {
-			// Edge case2: [=] directly after [op]
-			if (result) {
-				operand1 = result;
-				operand2 = result;
-			} else {
-				operand1 = operand;
-				operand2 = operand;
-			}
-		} else if (prevInput === 'equals') {
-			// Edge case3: [=] directly after [=]
-			operand1 = result;
-			operator = prevOperator;
-			operand2 = prevOperand;
-		} else {
-			// Else normal operation
-			operand2 = operand;
-		}
+    // Edge case1: New calculation of [=] directly after [num] with no operator
+    if (operator === 'none' && prevOperator === undefined) {
+      operator = prevOperator;
+      operand2 = prevOperand;
+      result = operand;
+      performCalculation = false;
+    } else if (prevInput === 'operator') {
+      // Edge case2: [=] directly after [op]
+      if (result) {
+        operand1 = result;
+        operand2 = result;
+      } else {
+        operand1 = operand;
+        operand2 = operand;
+      }
+    } else if (prevInput === 'equals') {
+      // Edge case3: [=] directly after [=]
+      operand1 = result;
+      operator = prevOperator;
+      operand2 = prevOperand;
+    } else {
+      // Else normal operation
+      operand2 = operand;
+    }
 
-		// If operand1 = operationQueue, omit operator since already captured in the queue
-		let ignoreOp = Array.isArray(operand1);
-		if (performCalculation) {
-			let calcObj = calculate(operand1, operator, operand2, ignoreOp);
-			result = calcObj.answer;
-			console.log(calcObj.formula);
-			// Add current calculation to calcTape
-			calcTape.push(calcObj.formula);
-		}
+    // If operand1 = operationQueue, omit operator since already captured in the queue
+    let ignoreOp = Array.isArray(operand1);
+    if (performCalculation) {
+      let calcObj = calculate(operand1, operator, operand2, ignoreOp);
+      result = calcObj.answer;
+      // console.log(calcObj.formula);
+      // Add current calculation to calcTape
+      calcTape.push(calcObj.formula);
+    }
 
-		this.setState({
-			operationQueue: [],
-			curOperation: {
-				operand1: operand1,
-				operator: 'none', // reset operator after operation
-				operand2: operand2
-			},
-			calcMem: {
-				result: result,
-				prevInput: 'equals',
-				prevOperator: operator,
-				prevOperand: operand2,
-				calcTape: calcTape
-			}
-		});
-	}
-	handleModifiedResult(updatedNum) {
-		// Check for valid #
-		if (isNaN(updatedNum)) updatedNum = 0;
+    this.setState({
+      operationQueue: [],
+      curOperation: {
+        operand1: operand1,
+        operator: 'none', // reset operator after operation
+        operand2: operand2,
+      },
+      calcMem: {
+        result: result,
+        prevInput: 'equals',
+        prevOperator: operator,
+        prevOperand: operand2,
+        calcTape: calcTape,
+      },
+    });
+  }
+  handleModifiedResult(updatedNum) {
+    // Check for valid #
+    if (isNaN(updatedNum)) updatedNum = 0;
 
-		// If [backspace] on a result, update the modified result
-		this.setState({
-			calcMem: {
-				result: updatedNum,
-				prevInput: 'number'
-			}
-		});
-	}
-	handleClear() {
-		let { calcTape } = this.state.calcMem;
-		this.setState({
-			operationQueue: [],
-			curOperation: {
-				operand1: 0,
-				operator: 'none',
-				operand2: 0
-			},
-			calcMem: {
-				result: null,
-				prevInput: null,
-				prevOperator: 'none',
-				prevOperand: null,
-				calcTape: calcTape
-			}
-		});
-	}
-	handleClearTape() {
-		let { operationQueue, curOperation, calcMem } = this.state;
-		let { operand1, operator, operand2 } = curOperation;
-		let { result, prevInput, prevOperator, prevOperand } = calcMem;
+    // If [backspace] on a result, update the modified result
+    this.setState({
+      calcMem: {
+        result: updatedNum,
+        prevInput: 'number',
+      },
+    });
+  }
+  handleClear() {
+    let { calcTape } = this.state.calcMem;
+    this.setState({
+      operationQueue: [],
+      curOperation: {
+        operand1: 0,
+        operator: 'none',
+        operand2: 0,
+      },
+      calcMem: {
+        result: null,
+        prevInput: null,
+        prevOperator: 'none',
+        prevOperand: null,
+        calcTape: calcTape,
+      },
+    });
+  }
+  handleClearTape() {
+    let { operationQueue, curOperation, calcMem } = this.state;
+    let { operand1, operator, operand2 } = curOperation;
+    let { result, prevInput, prevOperator, prevOperand } = calcMem;
 
-		this.setState({
-			operationQueue: operationQueue,
-			curOperation: {
-				operand1: operand1,
-				operator: operator,
-				operand2: operand2
-			},
-			calcMem: {
-				result: result,
-				prevInput: prevInput,
-				prevOperator: prevOperator,
-				prevOperand: prevOperand,
-				calcTape: []
-			}
-		});
-	}
-	render() {
-		return (
-			<div className="calc-wrapper">
-				<Inputs
-					onDigit={this.handleDigitInput}
-					onOperator={this.handleOperation}
-					onEquals={this.handleEquals}
-					onModifiedResult={this.handleModifiedResult}
-					onClear={this.handleClear}
-					onClearTape={this.handleClearTape}
-					calcMem={this.state.calcMem}
-					curOperation={this.state.curOperation}
-				/>
-			</div>
-		);
-	}
+    this.setState({
+      operationQueue: operationQueue,
+      curOperation: {
+        operand1: operand1,
+        operator: operator,
+        operand2: operand2,
+      },
+      calcMem: {
+        result: result,
+        prevInput: prevInput,
+        prevOperator: prevOperator,
+        prevOperand: prevOperand,
+        calcTape: [],
+      },
+    });
+  }
+  render() {
+    return (
+      <div className="calc-wrapper">
+        <Inputs
+          onDigit={this.handleDigitInput}
+          onOperator={this.handleOperation}
+          onEquals={this.handleEquals}
+          onModifiedResult={this.handleModifiedResult}
+          onClear={this.handleClear}
+          onClearTape={this.handleClearTape}
+          calcMem={this.state.calcMem}
+          curOperation={this.state.curOperation}
+        />
+      </div>
+    );
+  }
 }
 
 export default Calculator;
